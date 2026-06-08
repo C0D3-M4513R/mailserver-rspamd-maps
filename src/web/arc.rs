b1cdb3797a673771061479e77ef145fbfd47851b
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2026-06-09 01:00:57 +0200

    Use rspamd's custom base64 encoding

--- src/web/arc.rs
+++ src/web/arc.rs
@@ -3,6 +3,12 @@ use actix_web::http::StatusCode;
 use actix_web::HttpResponse;
 use actix_web::web::Data;
 
+//WHY a custom alphabet rspamd? WHY?!?
+static BASE64: data_encoding::Encoding = data_encoding_macro::new_encoding!{
+    symbols: "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/",
+    padding: '=',
+};
+
 #[actix_web::get("/signing_table")]
 pub async fn signing_table(db: Data<sqlx::postgres::PgPool>) -> HttpResponse {
     let res = sqlx::query!(r#"
@@ -41,7 +47,7 @@ WHERE public.dkim.active"#)
         Ok(v) => v,
     };
 
-    let res = String::from_iter(res.into_iter().map(|v|format!("{}-{} {} {} {}\n",v.domain_id, v.selector, v.name, v.selector, data_encoding::BASE64.encode(v.private_key.as_slice()))));
+    let res = String::from_iter(res.into_iter().map(|v|format!("{}-{} {} {} {}\n",v.domain_id, v.selector, v.name, v.selector, BASE64.encode(v.private_key.as_slice()))));
 
     HttpResponse::with_body(StatusCode::OK, res.boxed())
 }
